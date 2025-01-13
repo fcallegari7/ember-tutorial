264f73c0aa0337af5792254fd5343e2a81cf02f1
Fabio Callegari <fabiocallegari.it@gmail.com>
2025-01-13 11:45:13 -0800

    add sass

--- app/services/request-manager.js
+++ app/services/request-manager.js
@@ -2,13 +2,10 @@ import BaseRequestManager from '@ember-data/request';
 import Fetch from '@ember-data/request/fetch';
 import { JsonSuffixHandler } from 'super-rentals/utils/handlers';
 
-interface RequestManagerArgs {
-  use: any[];
-}
-
 export default class RequestManager extends BaseRequestManager {
-  constructor(args: RequestManagerArgs) {
+  constructor(args) {
     super(args);
+
     this.use([JsonSuffixHandler, Fetch]);
   }
 }
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     'ember-cli-babel': { enableTypeScriptTransform: true },
+    sassOptions: {
+      extension: 'scss'
+    }
   });
 
   return app.toTree();
